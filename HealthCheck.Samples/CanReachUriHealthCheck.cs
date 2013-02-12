0874ced4a79fd69f21ab67de7e00524b9c816eda
Ian Joyce <ian.joyce@gmail.com>
2013-02-11 21:39:58 -0600

    Adding another sample.

---
+++ HealthCheck.Samples/CanReachUriHealthCheck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using HealthCheck.Core;
+
+namespace HealthCheck.Samples
+{
+    public class CanReachUriHealthCheck : AbstractHealthCheck
+    {
+        private readonly string _uri;
+
+        public CanReachUriHealthCheck(string name) : base(name)
+        {
+        }
+
+        public CanReachUriHealthCheck(string name, string uri) : this(name)
+        {
+            if (string.IsNullOrWhiteSpace(uri))
+                throw new ArgumentNullException("uri");
+
+            _uri = uri;
+        }
+
+        public override Result Check()
+        {
+            var client = new HttpClient();
+            var response = client.GetAsync(_uri).Result;
+            return new Result(response.StatusCode == HttpStatusCode.OK, response.StatusCode.ToString(), null);
+        }
+    }
+}
