c31b28a6f7d2c56807372f7e0c3ba090c2810929
Ian Joyce <ian.joyce@gmail.com>
2013-02-11 22:14:24 -0600

    Adding tests for the samples.

---
+++ HealthCheck.Samples.Test/AlwaysTrueHealthCheckTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace HealthCheck.Samples.Test
+{
+    [TestFixture]
+    public class AlwaysTrueHealthCheckTests
+    {
+        [Test]
+        public void Check_WhenCalled_ReturnsTrue()
+        {
+            var result = new AlwaysTrueHealthCheck("Always True").Execute();
+            Assert.IsTrue(result.IsHealthy);
+        }
+    }
+}
---
+++ HealthCheck.Samples.Test/CanReachUriHealthCheckTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace HealthCheck.Samples.Test
+{
+    [TestFixture]
+    class CanReachUriHealthCheckTests
+    {
+        [Test]
+        public void Check_WithReachableUri_ReturnsHealthy()
+        {
+            var result = new CanReachUriHealthCheck("Google", "http://google.com/").Execute();
+            Assert.IsTrue(result.IsHealthy);
+        }
+
+        [Test]
+        public void Check_WithUnreachableUri_ReturnsNotHealthy()
+        {
+            var result = new CanReachUriHealthCheck("A URI That Does Not Work", "http://a-uri-that-does-not-work.com/").Execute();
+            Assert.IsFalse(result.IsHealthy);
+        }
+    }
+}
