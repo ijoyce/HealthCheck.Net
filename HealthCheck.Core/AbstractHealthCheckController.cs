492741055e386518f4138289a669cd8a6e6120e2
Ian Joyce <ian.joyce@gmail.com>
2013-02-16 22:33:31 -0600

    Starting a base mvc controller.

---
+++ HealthCheck.Core/AbstractHealthCheckController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace HealthCheck.Core
+{
+    public abstract class AbstractHealthCheckController : Controller
+    {
+        private readonly HealthCheckRegistry _healthCheckRegistry = new HealthCheckRegistry();
+
+        protected HealthCheckRegistry HealthCheckRegistry { get { return _healthCheckRegistry; } }
+
+        public ActionResult Index()
+        {
+            if (HealthCheckRegistry.Count() == 0)
+                return Json("No health checks are registered.");
+
+            return Json(_healthCheckRegistry.RunHealthChecks(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ HealthCheck.Sample.Mvc/Controllers/HealthCheckController.cs
@@ -0,0 +1,17 @@
+﻿using HealthCheck.Core;
+using HealthCheck.Samples;
+
+namespace HealthCheck.Sample.Mvc.Controllers
+{
+    public class HealthCheckController : AbstractHealthCheckController
+    {
+        public HealthCheckController()
+        {
+            HealthCheckRegistry.Register(new AlwaysTrueHealthCheck("Sample 1"));
+            HealthCheckRegistry.Register(new AlwaysTrueHealthCheck("Sample 2"));
+            HealthCheckRegistry.Register(new AlwaysTrueHealthCheck("Sample 3"));
+            HealthCheckRegistry.Register(new CanReachUriHealthCheck("Google", "http://google.com/"));
+            HealthCheckRegistry.Register(new CanReachUriHealthCheck("A bad uri", "http://a-uri-that-does-not-work.com"));
+        }
+    }
+}
